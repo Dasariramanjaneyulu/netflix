1a9850250c1b9a2cab93bee1bdc341c1e0878cd6
Dasariramanjaneyulu <124892569+Dasariramanjaneyulu@users.noreply.github.com>
2023-02-23 18:35:24 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let inputsearch_v = document.getElementById('inputsearch');
+
+inputsearch_v.addEventListener("click", ()=>{
+    let inputbox_v =document.getElementById('inputbox');
+    inputbox_v.style.display="block";
+
+    let email_v = document.getElementById('email');
+    email_v.style.fontsize="15px";
+    email_v.style.transition="2s";
+})
